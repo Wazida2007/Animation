ebe16fcdba7d0963e840b81693f0db4694b29dff
Wazida Tabbasum <135168053+Wazida2007@users.noreply.github.com>
2025-03-09 14:27:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export", // Enables static HTML export
-    basePath: "Animation", // Replace with your GitHub repository name
-    images: {
-      unoptimized: true, // Required for GitHub Pages
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
+
